4eca31e7d8d137042c7dad32a12c6fce053ffa62
gitsibi <159125779+gitsibi@users.noreply.github.com>
2025-01-31 09:38:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = 3000;
-const students = require('./data.json'); 
+const studentsData = require('./data.json'); 
 
 app.use(express.json());
 
